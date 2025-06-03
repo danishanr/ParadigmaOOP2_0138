0600849161a35f615ea731e3ea0cc606272d8050
danishanr <danishaanr@gmail.com>
2025-06-03 17:41:24 +0700

    Merapikan kode agar lebih readable

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -2,26 +2,26 @@
 using namespace std;
 
 class seseorang {
-    public : 
-        virtual void pesan() = 0;
-        // virtual void pesan() {
-        //      cout << "Pesan dari seseorang" << endl;
-        //
-        // }
+public : 
+    virtual void pesan() = 0;
+    // virtual void pesan() {
+    //      cout << "Pesan dari seseorang" << endl;
+    //
+    // }
 };
 
 class joko : public seseorang {
-    public :
-        void pesan() {
-            cout << "Pesan dari joko" << endl;
-        }
+public :
+    void pesan() {
+        cout << "Pesan dari joko" << endl;
+    }
 };
 
 class lia : public seseorang {
-    public :
-        void pesan() {
-            cout << "Pesan dari Lia" << endl;
-        }
+public :
+    void pesan() {
+        cout << "Pesan dari Lia" << endl;
+    }
 };
 
 int main() {
